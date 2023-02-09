ea2daa982720f24b5310f030771e877fcd919a20
Truman Miller <wesleythemiller@gmail.com>
2023-02-08 19:03:38 -0600

    started testing

--- __tests__/middleware.js
+++ __tests__/middleware.js
@@ -3,8 +3,21 @@ const db = require('../server/models/chatroomModels.js');
 
 const server = 'http://localhost:3000';
 
+// run the server while you are testing
+
 describe('middleware tests', () => {
   describe('/check', () => {
-
+    describe('GET', () => {
+      it('responds with 200 status and current leaderboard', () =>
+        request(server)
+          .get('/check')
+          .expect('Content-Type', /application\/json/)
+          .expect((res) => {
+            expect(res.body.first_place).toBeDefined()
+            expect(res.body.second_place).toBeDefined()
+            expect(res.body.third_place).toBeDefined()
+          })
+          .expect(200));
+    });
   });
 });
